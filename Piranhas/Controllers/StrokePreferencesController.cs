a026adbafa1bcded95346236d20019381f4d2360
kkyrazis <kkyrazis@ufl.edu>
2015-04-24 23:32:25 -0400

    Working on Stroke Preference tabs

---
+++ Piranhas/Controllers/StrokePreferencesController.cs
@@ -0,0 +1,22 @@
+﻿using Piranhas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace Piranhas.Controllers
+{
+    public class StrokePreferencesController : Controller
+    {
+        SwimmerContext db = new SwimmerContext();
+        // GET: StrokePreferences
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            ViewBag.Swimmers = db.Swimmers.ToList().FindAll(sw => sw.UserID == userId);
+            return View();
+        }
+    }
+}
